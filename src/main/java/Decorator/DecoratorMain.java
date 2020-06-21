c6bb16fda0fb561066d25f4d6402e5d701aae539
Rabeya Zarfeshan <rabeya31@gmail.com>
2020-06-21 14:12:33 +0530

    checkin

--- src/main/java/Decorator/DecoratorMain.java
+++ src/main/java/Decorator/DecoratorMain.java
@@ -4,7 +4,7 @@ public class DecoratorMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Rabeya");
+		System.out.println("Rabeya");
 	}
 
 }
